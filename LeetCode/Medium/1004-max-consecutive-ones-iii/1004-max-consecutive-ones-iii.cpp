9a3af927015d2886131ceea0e5fc71aecc04cd2a
Chaitanya More <105599113+chaitanyamore003@users.noreply.github.com>
2026-09-22 00:16:12 +0530

    Time: 0 ms (100%), Space: 69.7 MB (5.27%) - LeetHub

---
+++ LeetCode/Medium/1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+
+        // we create a variable size sliding window to track frequencies or zero
+        // if current sliding window has k number of zero's then we can convert
+        // them to ones
+        int n = nums.size();
+        int l = 0; // left
+        int zeros = 0;
+        int ans = 0;
+
+        // r -> right
+        for (int r = 0; r < n; r++) {
+            if (nums[r] == 0)
+                zeros++;
+
+            while (zeros > k) {
+                if (nums[l] == 0)
+                    zeros--;
+                l++;
+            }
+            ans = max(ans, r - l + 1);
+        }
+
+        return ans;
+    }
+};
