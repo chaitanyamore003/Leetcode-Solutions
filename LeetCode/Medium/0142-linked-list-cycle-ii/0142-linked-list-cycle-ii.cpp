eee9dd1d061c198bb04029c279f3a3f20da30eeb
Chaitanya More <105599113+chaitanyamore003@users.noreply.github.com>
2026-09-22 01:10:58 +0530

    Time: 9 ms (47.16%), Space: 11.2 MB (85.28%) - LeetHub

---
+++ LeetCode/Medium/0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* detectCycle(ListNode* head) {
+        // optimal
+
+        // using two pointers slow and fast
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            // cycle exists
+            if (slow == fast) {
+                // reset slow
+                slow = head;
+
+                // finding the enterance of the cycle
+                // now both pointers move one step at a time
+                while (slow != fast) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+
+                // they both meet on the enterance of the cycle hence return any
+                // of them
+                return slow;
+            }
+        }
+        return nullptr;
+    }
+};
