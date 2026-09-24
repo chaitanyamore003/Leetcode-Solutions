0c0a17d651e93eebe6f91c318da3c740f7107c06
Chaitanya More <105599113+chaitanyamore003@users.noreply.github.com>
2026-09-24 20:57:56 +0530

    Time: 0 ms (100%), Space: 8.1 MB (98.13%) - LeetHub

--- 4000-largest-integer-with-given-digit-sum/4000-largest-integer-with-given-digit-sum.cpp
+++ 4000-largest-integer-with-given-digit-sum/4000-largest-integer-with-given-digit-sum.cpp
@@ -1,23 +1,28 @@
 class Solution {
-private:
-    int getDigitsSum(int n) {
-        int sum = 0;
-        while (n) {
-            sum += n % 10;
-            n /= 10;
-        }
-        return sum;
-    }
-
 public:
     int largestInteger(int n, int s) {
-        int e = pow(10, n)-1;
 
-        while (e >= 0) {
-            if (getDigitsSum(e) == s)
-                return e;
-            e--;
+        // If s is impossible to achieve, return -1
+        if (s > n * 9)
+            return -1;
+
+        // To store the answer
+        int ans = 0;
+
+        // Run until all n digits are constructed
+        for (int i = 0; i < n; i++) {
+
+            // To create the largest integer,
+            // take the largest possible digit
+            int digit = min(9, s);
+
+            // Build the answer
+            ans = ans * 10 + digit;
+
+            // Remove the selected digit's contribution
+            s -= digit;
         }
-        return -1;
+
+        return ans;
     }
 };
